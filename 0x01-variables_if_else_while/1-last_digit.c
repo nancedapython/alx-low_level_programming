4e9d604c631e3e6c248e17c2503ca0d56a75babd
nancedapython <nancyogbedu@gmail.com>
2022-12-10 00:23:32 -0800

    Learning hard thing

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,18 +15,15 @@ int main(void)
 	printf("Last digit of %i is %i and is ", n, (n % 10));
 	if ((n % 10) > 5)
 	{
-		printf("Last digit of %i is %i  and is greater than 5\n",
-				n, (n % 10));
+		printf("greater than 5\n");
 	}
 	else if ((n % 10) < 6 && (n % 10) != 0)
 	{
-		printf("Last digit of %i is %i and is less than 6 and not 0\n",
-				n, (n % 10));
+		printf("less than 6 and not 0\n");
 	}
 	else
 	{
-		printf("Last digit of %i is %i and is 0\n",
-				n, (n % 10));
+		printf("0\n");
 	}
 	return (0);
 }
