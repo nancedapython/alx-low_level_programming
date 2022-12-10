31f3defe3a0a3e85d9a79ae707291c27805cd11b
nancedapython <nancyogbedu@gmail.com>
2022-12-09 21:49:30 -0800

    My commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,9 @@
-#include <stdlib.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /**
  * main - prints if the number is positive, negative or zero
- * Return: Always 0
+ * Return: Always 0 (Success) 
  */
 int main(void)
 {
