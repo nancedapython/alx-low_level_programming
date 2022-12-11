4e67df3426d6c08eae7c48d03241b9a6e9056365
nancedapython <nancyogbedu@gmail.com>
2022-12-11 07:59:15 -0800

    Thanks to you

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,41 +1,34 @@
 #include <stdio.h>
 
-
 /**
- * main - prints all possible different combinations of three digits                                                                                    *
+ * main - prints all possible different combinations of three digits
+ *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-
 	int n, m, l;
 
 	for (n = 48; n < 58; n++)
-        {
+	{
 		for (m = 49; m < 58; m++)
-
 		{
 			for (l = 50; l < 58; l++)
-
 			{
 				if (l > m && m > n)
-
 				{
-				  putchar(n)
+					putchar(n);
 
 					putchar(m);
 
-					putchar(l)
+					putchar(l);
 
 					if (n != 55 || m != 56)
-
 					{
-	       	          	       	 putchar(',');
+						putchar(',');
 
 						putchar(' ');
-
-
 					}
 				}
 
@@ -43,7 +36,6 @@ int main(void)
 		}
 
 	}
-
 	putchar('\n');
 
 	return (0);
