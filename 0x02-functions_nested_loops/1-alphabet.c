15798f5f421352180a1a7709502ea8c272e9ea40
nancedapython <nancyogbedu@gmail.com>
2022-12-13 14:53:34 -0800

    Alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - lowercase alphabet
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+_putchar('\n');
+}
