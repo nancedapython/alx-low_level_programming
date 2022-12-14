3f5b1d7a81a6681c65ece6ce5f96470d83fcb5ec
nancedapython <nancyogbedu@gmail.com>
2022-12-14 05:06:08 -0800

    Print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: The integer to be computed.
+ * Return: value of the last digit
+ */
+
+int print_last_digit(int n)
+{
+	int last_digit;
+
+	if (n >= 0)
+	{
+		last_digit = n % 10;
+		_putchar(last_digit + '0');
+		return (last_digit);
+	}
+	else
+	{
+		last_digit = -(n % 10);
+		_putchar(last_digit + '0');
+		return (last_digit);
+	}
+
+}
