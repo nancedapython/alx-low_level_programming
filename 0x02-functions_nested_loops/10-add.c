3b1762e288411660e689650556602977c8d8f9d4
nancedapython <nancyogbedu@gmail.com>
2022-12-14 05:37:52 -0800

    Add ons

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add two integers and returns the result
+ * @num1: first integer to be computed
+ * @num2: second integer to be computed
+ * Return: sum of the two integers
+ */
+
+int add(int num1, int num2)
+
+{
+		return (num1 + num2);
+}
