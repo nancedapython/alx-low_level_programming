cc6029839ed2e7efac5295601291e7bbffada75f
nancedapython <nancyogbedu@gmail.com>
2022-12-14 04:04:14 -0800

    Characterization

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet 10x in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
