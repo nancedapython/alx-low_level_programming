356db195b533ae53ca206842bbbbdd60c030bffc
nancedapython <nancyogbedu@gmail.com>
2022-12-15 22:02:33 -0800

    Uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase characters
+ * @c: value to be computed
+ * Return: 1 if uppercase otherwise 0.
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+}
