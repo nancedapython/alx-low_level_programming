68590c3138dc24446d2fcb156fb11d66c756c07b
nancedapython <nancyogbedu@gmail.com>
2022-12-15 23:28:00 -0800

    Digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isdigit - function that verifies if a charcter is a digit or not
+ * @c: tested character
+ * Return: returns function 1 if it is uppercase, 0 if noy
+ */
+int _isdigit(int c)
+{
+if ((c >= 48 && (c <= 57))
+return (1);
+return (0);
+}
