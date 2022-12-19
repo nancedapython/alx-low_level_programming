0398b1bf9932fb62383419d85b12ab3cf8f07c01
nancedapython <nancyogbedu@gmail.com>
2022-12-19 13:13:59 -0800

    Strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,12 +3,13 @@
 /**
 * _strlen - Function that returns the length of a string
 * @s: this is the input string
-* Return: Length of string
+* Return: Length of a string
 */
 int _strlen(char *s)
 {
-	int index;
+	int count = 0;
 
-	for (index = 0; s[index] != '\0'; index++)
-	return (index);
+	while (*(s + count) != '\0';)
+		count++;
+	return (count);
 }
