7f2ef81824ad5d00711cee3a9fd86e645775839e
nancedapython <nancyogbedu@gmail.com>
2022-12-19 13:30:02 -0800

    Strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* _strlen - Function that returns the length of a string
+* _strlen - returns the length of a string
 * @s: this is the input string
 * Return: Length of a string
 */
@@ -9,7 +9,7 @@ int _strlen(char *s)
 {
 	int count = 0;
 
-	while (*(s + count) != '\0';)
+	while (*(s + count) != '\0')
 		count++;
 	return (count);
 }
