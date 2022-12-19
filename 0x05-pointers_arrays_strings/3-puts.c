e6c8bddb88fd29d932fd72c1f3f2836cc39997a2
nancedapython <nancyogbedu@gmail.com>
2022-12-19 14:03:05 -0800

    Strings

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
-*_puts - prints a string, followed by a new line
+* _puts - prints a string, followed by a new line
 * @str: pointer to the string to print
 * Return: void
 */
+
 void _puts(char *str)
 {
 	int i = 0;
