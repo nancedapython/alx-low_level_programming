1f74482a4cfef0a27ffc4b5696cbb8650e89d6dc
nancedapython <nancyogbedu@gmail.com>
2022-12-19 14:41:36 -0800

    stringing

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
- *_puts - prints a string
- *@str: value to be entered
+ * _puts - prints a string
+ * @str: value to be entered
  */
 
 void _puts(char *str)
 {
-	int i = 0;
+int i = 0;
 
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
-
-	_putchar('\n');
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
 }
