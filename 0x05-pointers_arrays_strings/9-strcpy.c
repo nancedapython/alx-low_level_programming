75ea5cbadec5c0ffd4988a54f8cb4acbabbb4d9a
nancedapython <nancyogbedu@gmail.com>
2022-12-19 18:18:18 -0800

    More to point

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,48 +1,16 @@
 #include "main.h"
 
 /**
- * _strcpy - copies the string pointed to by src,
- *including the terminating null byte, to the buffer
- *pointed to by dest
- * @dest: This is destination
- * @src: This is the source
- *
- * Return: The pointer to dest
+ *_strcpy - copies string
+ *@dest: where to copy string to
+ *@src: what to be copied
+ *Return: dest
  */
 char *_strcpy(char *dest, char *src)
 {
-	char "start = dest;
+	int i;
 
-	while (src != '\0')
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
-	*dest = '\0';
-	return (start);
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	return (dest);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-8
