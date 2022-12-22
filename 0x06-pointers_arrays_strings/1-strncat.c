a51fadd1cb7fbc79948a31fdb25ae35a509bb646
nancedapython <nancyogbedu@gmail.com>
2022-12-22 07:33:39 -0800

    cats

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_strncat - concatenates two strings
+ *@dest: destination of concatenated string
+ *@src: source of text to be concatenated
+ *@n: number of text to be concatenated
+ *Return: result of concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+
+	return (strncat(dest, src, n));
+}
