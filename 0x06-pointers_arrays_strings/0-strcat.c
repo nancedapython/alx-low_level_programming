7fefe94a222d703ce37de6d92807978db3b9f161
nancedapython <nancyogbedu@gmail.com>
2022-12-22 06:41:18 -0800

    More pointers

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *_strcat - concatenates two strings
+ *@dest: destination of concatenated text
+ *@src: source of concatenated text
+ *Return: results of concatenated text
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *s = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (s);
+}
