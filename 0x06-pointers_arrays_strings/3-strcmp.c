ac2793d5ce3ff3a0c88ea463993182add6b9f80d
nancedapython <nancyogbedu@gmail.com>
2022-12-22 08:21:29 -0800

    compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,9 +4,11 @@
  * _strcmp - compares two strings
  * @s1: first string.
  * @s2: second string.
- * Return: result of comparison.
+ * Return: 0 if s1 and s2 are equals,
+ * another number if not.
  */
-int _strcmp(char *s1, char *s2);
+
+int _strcmp(char *s1, char *s2)
 {
 	int i = 0, op = 0;
 
