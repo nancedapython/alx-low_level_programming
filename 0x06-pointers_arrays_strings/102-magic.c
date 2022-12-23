649c9d6eaf1b83020689f425c08234a4965eaa67
nancedapython <nancyogbedu@gmail.com>
2022-12-23 00:49:37 -0800

    Description

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,6 +8,7 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
+
 	/*
 	 * write your line of code here...
 	 * Remember:
@@ -18,7 +19,6 @@ int main(void)
 	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
-
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
