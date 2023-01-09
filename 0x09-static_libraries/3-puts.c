8023a297785143dd551fb662eb1c1fa799701b24
nancedapython <nancyogbedu@gmail.com>
2023-01-09 14:54:21 -0800

    libraries

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include<stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+void _put(char *s)
+{
+	puts(s);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+Task 0x09. C static libraries
