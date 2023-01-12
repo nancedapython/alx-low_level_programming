bf8bfd1cca49635244d72d0b3458c37b45735fd3
nancedapython <nancyogbedu@gmail.com>
2023-01-12 05:57:17 -0800

    add memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *_strdup -  returns a pointer to a newly allocated space in memory
+ *@str: string
+ *Return:  returns a pointer to a newly allocated space in memory
+ */
+
+char *_strdup(char *str)
+{
+	char *duplicate;
+	int index, len = 0;
+
+	if (str == NULL)
+
+		return (NULL);
+	for (index = 0; str[index]; index++)
+		len++;
+
+	duplicate = malloc(sizeof(char) * (len + 1));
+
+	if (duplicate == NULL)
+
+		return (NULL);
+	for (index = 0; str[index]; index++)
+		duplicate[index] = str[index];
+	duplicate[len] = '\0';
+	return (duplicate);
+}
