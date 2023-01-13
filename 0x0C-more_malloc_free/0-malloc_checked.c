b19d944fe494986a87addbc1157b3e380f77300c
nancedapython <nancyogbedu@gmail.com>
2023-01-13 05:25:55 -0800

    share memory

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,19 +1,20 @@
 #include <stdlib.h>
 #include "main.h"
-
 /**
  * malloc_checked - allocates memory using malloc
- * @b: number of bytes to allocate
+ * @b: number of bytes to allocates
  *
- * Return: a pointer to the allocated memory
+ * Return: a pointer to the allocated memory.
+ * if malloc fails, status value is equal to 98.
  */
 
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+	char *p;
 
-	ptr = malloc(b);
-	if (ptr == NULL)
+	p = malloc(b);
+	if (p == NULL)
 		exit(98);
-	return (ptr);
+
+	return (p);
 }
