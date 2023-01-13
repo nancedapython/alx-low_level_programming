ba7380570fe2bd9d99a92177800b84c9dfc33777
nancedapython <nancyogbedu@gmail.com>
2023-01-13 07:36:52 -0800

    memory

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,9 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdlib.h>
-#include <limits.h>
 
-
-_checked(unsigned int b);void *malloc_checked(unsigned int b);
+void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 #endif
