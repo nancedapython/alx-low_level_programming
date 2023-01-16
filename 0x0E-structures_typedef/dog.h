1875e051a1cdb9314bc7744c3a5ce491ac4b9ecb
nancedapython <nancyogbedu@gmail.com>
2023-01-16 15:08:22 -0800

    dogs are friendly

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,13 +2,13 @@
 #define DOG_H
 
 /**
- * struct dog - a dog's basic info
- * @name: First member
- * @age: Second member
- * @owner: Third member
- *
- * Description: Longer description
- */
+  * struct dog - a dog's basic info
+  * @name: First member
+  * @age: Second member
+  * @owner: Third member
+  *
+  * Description: Longer description
+  */
 
 struct dog
 {
@@ -16,11 +16,11 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
-/**
- * dog_t - typedef for struct dog
- */
+};
 
+/**
+  * dog_t - typedef for struct dog
+  */
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
