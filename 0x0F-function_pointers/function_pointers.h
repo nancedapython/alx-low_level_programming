6588519df5659e89e21faa4eff7f0f0f4e5b1bbe
nancedapython <nancyogbedu@gmail.com>
2023-01-19 14:19:53 -0800

    prints

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,7 +2,7 @@
 #define FUNCTION_POINTERS_H
 #include <stddef.h>
 #include <stdlib.h>
-
+int _putchar(char c)
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
